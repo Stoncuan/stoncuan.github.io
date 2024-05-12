482fae30afed3e6e14585f7404f5340f470f2188
Stoncuan <smacpalbawangun@gmail.com>
2024-05-12 10:38:25 +0900

    feat: push data

---
+++ js/scirpt.js
@@ -0,0 +1,27 @@
+flag = 1;
+
+function f1() {
+  alert("Makasih ya udah maafin aku!");
+}
+
+function f() {
+  if (flag == 1) {
+    Bn.style.top = 525;
+
+    Bn.style.left = 300;
+
+    flag = 2;
+  } else if (flag == 2) {
+    Bn.style.top = 525;
+
+    Bn.style.left = 50;
+
+    flag = 3;
+  } else if (flag == 3) {
+    Bn.style.top = 525;
+
+    Bn.style.left = 166;
+
+    flag = 1;
+  }
+}
